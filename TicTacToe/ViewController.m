5faf788dabfde05cab2e137ca149665f30d63cbe
David Kang <kdragom@Davids-MacBook-Pro.local>
2016-01-15 13:33:33 -0800

    mainstory

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -25,11 +25,14 @@
 
 @implementation ViewController
 
+
+//declaring number of moves equals to 0
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.numberOfMoves = 0;
 }
 
+//if the buttontapped's #of move is less than 9, computer puts value.
 - (IBAction)onPlayButtonTapped:(UIButton *)sender {
     [self enterPlayerMove:sender];
     if(self.numberOfMoves < 9){
@@ -44,19 +47,21 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+    //calling enterplayermove and then blue color x mark will be shown when you press the button
 - (void)enterPlayerMove:(UIButton *) sender {
     [sender setTitle:@"X" forState:UIControlStateNormal];
     [sender setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
     
+    //it shows "computer's turn" on the screen and number of moves will increase by 1
     self.whichPlayerLabel.text = @"Computer's turn";
     self.numberOfMoves++;
 }
 
-
+    //
 - (void)enterComputerValue {
     BOOL enteredValue = false;
     
+    //when value is entered, random value
     while (!enteredValue) {
         int randomNumber = (arc4random() % 9) + 100;
         UIButton *button = (UIButton *)[self.view viewWithTag:randomNumber];
